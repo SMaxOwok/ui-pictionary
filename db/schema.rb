9150327fd755118e7276f2523ff2ef756385a93d
Max Ono <ono.m@husky.neu.edu>
2019-11-21 16:41:35 -0800

    [CHORE] Enable pg_crypto extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_071450) do
+ActiveRecord::Schema.define(version: 2019_11_19_071455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
