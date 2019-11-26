d8ad9319ab840871a3c158a74963d7665319b2c8
Max Ono <max@userinterviews.com>
2019-11-26 12:19:29 -0800

    [CHORE] Add placeholder components for other states

--- app/javascript/components/game_states/index.js
+++ app/javascript/components/game_states/index.js
@@ -1,9 +1,13 @@
 import Initialized from './Initialized';
 import Setup from './Setup';
 import Completed from './Completed';
+import PreDraw from './PreDraw';
+import Drawing from './Drawing';
 
 export default {
   Initialized,
   Setup,
-  Completed
+  Completed,
+  PreDraw,
+  Drawing
 }
