d4359b5b66685a422cd640b9b48da2b5d667ae41
Max Ono <max@userinterviews.com>
2019-12-03 21:10:57 -0800

    [CHORE] Add roundUtils methods

--- app/javascript/components/Jumbotron/prompts/PreDraw.js
+++ app/javascript/components/Jumbotron/prompts/PreDraw.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import roundUtils from 'utils/roundUtils';
+
 export default class PreDraw extends Component {
   static propTypes = {
     currentUser: PropTypes.object,
@@ -11,30 +13,12 @@ export default class PreDraw extends Component {
     return this.props.game.currentRound;
   }
 
-  get isArtist() {
-    if (!this.props.currentUser) return false;
-
-    return this.currentRound.artist === this.props.currentUser.id;
-  }
-
-  get isOnDrawingTeam() {
-    if (!this.props.currentUser) return false;
-
-    return this.currentRound.team === this.props.currentUser.teamId;
-  }
-
-  get isOnTeam() {
-    if (!this.props.currentUser) return false;
-
-    return !!this.props.currentUser.teamId;
-  }
-
   get prompt() {
-    if (!this.isOnTeam) return 'Join a team to play';
+    if (!roundUtils.isOnTeam(this.currentRound, this.props.currentUser)) return 'Join a team to play';
 
-    if (this.isArtist) return 'Get ready to draw';
+    if (roundUtils.isArtist(this.currentRound, this.props.currentUser)) return 'Get ready to draw';
 
-    if (this.isOnDrawingTeam) return 'Get ready to guess';
+    if (roundUtils.isOnDrawingTeam(this.currentRound, this.props.currentUser)) return 'Get ready to guess';
 
     return 'Your team is up next round'
   }
