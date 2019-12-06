845afcb0df2a654e7aa755697de5da934b8f933d
Max Ono <max@userinterviews.com>
2019-12-06 12:26:01 -0800

    [FEAT] Indicate current artist in player list
    Closes #7

--- app/javascript/components/Player.js
+++ app/javascript/components/Player.js
@@ -3,27 +3,50 @@ import { connect } from 'react-redux';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-regular-svg-icons';
+
 import get from 'lodash/get';
+
+import roundUtils from 'utils/roundUtils';
 
 class Player extends React.Component {
   static mapStateToProps = (state, ownProps) => (
     {
+      game: get(state, 'entities.game'),
       guess: get(state, `guesses.${ownProps.player.id}`),
       team: get(state, `entities.team.${ownProps.player.teamId}`),
     }
   );
 
   static propTypes = {
-    player: PropTypes.object.isRequired
+    game: PropTypes.object.isRequired,
+    player: PropTypes.object.isRequired,
+    team: PropTypes.object.isRequired,
+    guess: PropTypes.object
   };
 
   get player() {
     return this.props.player;
   }
 
+  get currentRound() {
+    return this.props.game.currentRound;
+  }
+
+  get isArtist() {
+    return roundUtils.isArtist(this.currentRound, this.player);
+  }
+
   get guessClasses() {
     return classnames('Player__guess', {
       'Player__guess--correct': this.props.guess && this.props.guess.isCorrect,
+    });
+  }
+
+  get playerClasses() {
+    return classnames('Player', {
+      'Player--artist': this.isArtist
     });
   }
 
@@ -37,8 +60,12 @@ class Player extends React.Component {
     if (!this.player) return null;
 
     return (
-      <li className='Player'>
+      <li className={this.playerClasses}>
         {this.player.name}
+
+        {this.isArtist && (
+          <FontAwesomeIcon icon={faEdit} />
+        )}
 
         {this.guessedWord && (
           <span key={this.guessedWord} className={this.guessClasses}>
