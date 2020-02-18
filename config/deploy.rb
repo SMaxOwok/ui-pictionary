97cad34b207d467acb63268e1c93dd3af36aa694
Max Ono <max@userinterviews.com>
2020-02-18 11:09:40 -0800

    [FEAT] Enable deployment through capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.12.0"
+
+set :application, 'ui-pictionary'
+set :repo_url, 'git@github.com:SMaxOwok/ui-pictionary.git'
+
+# Default branch is :master
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, 'config/master.key', 'config/database.yml'
+
+# Copy master.key onto server, if not present
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+set :keep_releases, 2
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
