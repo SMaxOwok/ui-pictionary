52d4a436d0e32ada1e526134684ff03b7b40cb44
Max Ono <max@userinterviews.com>
2020-03-25 11:33:40 -0700

    [CHORE] Use singular resource name for leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,11 +1,11 @@
-class LeaderboardsController < ApplicationController
+class LeaderboardController < ApplicationController
   def show
     render json: {
       guessers: ActiveModelSerializers::SerializableResource.new(
         Player.top_guessers, each_serializer: PlayerSerializer
       ),
       drawers: ActiveModelSerializers::SerializableResource.new(
-          Player.top_drawers, each_serializer: PlayerSerializer
+        Player.top_drawers, each_serializer: PlayerSerializer
       )
     }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post 'ready', to: 'me#set_ready', as: :set_ready
   post 'unready', to: 'me#set_unready', as: :set_unready
 
-  resource :leaderboard, only: [:show]
+  resource :leaderboard, only: [:show], controller: 'leaderboard'
   resource :sessions, only: [:create, :destroy]
 
   mount ActionCable.server, at: '/cable'
